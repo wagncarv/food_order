89c7913453f56c9a14b9ede344e3029064c61c22
wagner carvalho <wagncarv@gmail.com>
2022-03-05 16:01:02 -0300

    correcao formatacao

--- lib/food_order_web/live/admin/products/form/form.ex
+++ lib/food_order_web/live/admin/products/form/form.ex
@@ -6,19 +6,22 @@ defmodule FoodOrderWeb.Admin.Products.Form do
   def update(assigns, socket) do
     IO.inspect(assigns)
     changeset = Products.change_product()
+
     {:ok,
-      socket
-      |> assign(assigns)
-      |> assign(changeset: changeset)
-      |> assign(product: %Product{})
-    }
+     socket
+     |> assign(assigns)
+     |> assign(changeset: changeset)
+     |> assign(product: %Product{})}
   end
 
   def handle_event("validate", %{"product" => product_params}, socket) do
     IO.inspect(product_params)
-    changeset = socket.assigns.product
-    |> Products.change_product(product_params)
-    |> Map.put(:action, :validate)
+
+    changeset =
+      socket.assigns.product
+      |> Products.change_product(product_params)
+      |> Map.put(:action, :validate)
+
     {:noreply, assign(socket, :changeset, changeset)}
   end
 end
